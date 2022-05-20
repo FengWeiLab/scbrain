e8897816c32cbf2750b5933f36624c5eefef8c0e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-05-20 17:21:43 -0400

    integration cluster

--- src/02-integration-cluster.R
+++ src/02-integration-cluster.R
@@ -1,202 +1,70 @@
+# Metainfo ----------------------------------------------------------------
 
-# Integration -------------------------------------------------------------
-project_sc <- readr::read_rds(file ="data/rda/project_sc.rds.gz")
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: Thu May 19 18:08:35 2022
+# @DESCRIPTION: integrate data and cluster
 
+# Library -----------------------------------------------------------------
 
-sc_list <- project_sc$sc
-names(sc_list) <- project_sc$project
+library(magrittr)
+library(ggplot2)
+library(rlang)
+library(patchwork)
+library(Seurat)
 
-features <- Seurat::SelectIntegrationFeatures(
-  object.list = sc_list,
-  nfeatures = 3000
-)
-sc_list <- Seurat::PrepSCTIntegration(
-  object.list = sc_list,
-  anchor.features = features
-)
-anchors <- Seurat::FindIntegrationAnchors(
-  object.list = sc_list,
-  normalization.method = "SCT",
-  anchor.features = features
-)
-sc_sct <- Seurat::IntegrateData(
-  anchorset = anchors,
-  normalization.method = "SCT"
-)
+# Load data ---------------------------------------------------------------
 
-readr::write_rds(
-  x = sc_sct,
-  file = "analysis/2022-02-08-single-cell/rda/sc_sct.rds.gz"
-)
-
-# Cluster -----------------------------------------------------------------
+project_sct <- readr::read_rds(file = "data/rda/project_sc_sct.rds.gz")
 
 
-sc_sct %>%
-  Seurat::RunPCA() %>%
-  Seurat::RunUMAP(reduction = "pca", dims = 1:30) %>%
-  Seurat::RunTSNE(reduction = "pca", dims = 1:30) %>%
-  Seurat::FindNeighbors(reduction = "pca", dims = 1:30) %>%
-  Seurat::FindClusters(resolution = 0.4) ->
-  sc_sct_cluster
+# Function ----------------------------------------------------------------
 
-readr::write_rds(
-  x = sc_sct_cluster,
-  file = "analysis/2022-02-08-single-cell/rda/sc_sct_cluster.rds.gz"
-)
-
-
-Seurat::DimPlot(
-  sc_sct_cluster,
-  reduction = "tsne",
-  label = TRUE,
-  repel = TRUE
-)
-
-
-
-# Cell annotation ---------------------------------------------------------
-
-
-# sc-Type -----------------------------------------------------------------
-
-
-library(HGNChelper)
-# load gene set preparation function
-source("https://raw.githubusercontent.com/chunjie-sam-liu/sc-type/master/R/gene_sets_prepare.R")
-# load cell type annotation function
-source("https://raw.githubusercontent.com/chunjie-sam-liu/sc-type/master/R/sctype_score_.R")
-# DB file
-db_ = "https://raw.githubusercontent.com/chunjie-sam-liu/sc-type/master/ScTypeDB_full.xlsx";
-tissue = "Immune system" # e.g. Immune system, Liver, Pancreas, Kidney, Eye, Brain
-gs_list_immune_system = gene_sets_prepare(db_, "Immune system")
-gs_list_brain = gene_sets_prepare(db_, "Brain")
-
-gs_list_immune_system %>%
-  purrr::map(.f = function(.x) {
-    names(.x) <- paste(names(.x), "(Immune cell)")
-    .x
-  }) ->
-  gs_list_immune_system
-gs_list_brain %>%
-  purrr::map(.f = function(.x) {
-    names(.x) <- paste(names(.x), "(Brain)")
-    .x
-  }) ->
-  gs_list_brain
-
-gs_list <- list(
-  gs_positive = c(
-    gs_list_immune_system$gs_positive,
-    gs_list_brain$gs_positive
-  ),
-  gs_negative = c(
-    gs_list_immune_system$gs_negative,
-    gs_list_brain$gs_negative
+fn_stat_cell <- function(.x, .y) {
+  # .x <- project_sct$sc[[1]]
+  # .y <- project_sct$sct[[1]]
+  
+  .xd <- .x@meta.data
+  .yd <- .y@meta.data
+  
+  .n_x_cells <- nrow(.xd)
+  .mean_reads_per_cell <- mean(.xd$nCount_RNA)
+  .median_gene_per_cell <- median(.xd$nFeature_RNA)
+  .n_y_cells <- nrow(.yd)
+  
+  tibble::tibble(
+    `estimated number of cells` = .n_x_cells,
+    `mean reads per cell` = .mean_reads_per_cell,
+    `median genes per cell` = .median_gene_per_cell,
+    `number of cells after filtering` = .n_y_cells
   )
-)
-
-es.max <- sctype_score(
-  scRNAseqData = sc_sct_cluster[["SCT"]]@scale.data,
-  scaled = TRUE,
-  gs = gs_list$gs_positive,
-  gs2 = gs_list$gs_negative
-)
-
-cL_results <- do.call(
-  "rbind",
-  lapply(
-    unique(sc_sct_cluster@meta.data$seurat_clusters),
-    function(cl) {
-      es.max.cl <- sort(
-        rowSums(es.max[, rownames(sc_sct_cluster@meta.data[sc_sct_cluster@meta.data$seurat_clusters == cl, ])]),
-        decreasing = TRUE
-      )
-      head(
-        data.frame(
-          cluster = cl,
-          type = names(es.max.cl),
-          scores = es.max.cl,
-          ncells = sum(sc_sct_cluster@meta.data$seurat_clusters == cl)
-        ),
-        10
-      )
-    }
-  )
-)
-
-cL_results %>%
-  dplyr::group_by(cluster) %>%
-  tidyr::nest() %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(mm = purrr::map(
-    .x = data,
-    .f = function(.x) {
-      .x %>%
-        dplyr::mutate(
-          cell = gsub(
-            pattern = " \\(.*\\)",
-            replacement = "",
-            x = type
-          )
-        ) %>%
-        dplyr::mutate(
-          tissue = gsub(
-            pattern = ".*\\(|\\)",
-            replacement = "",
-            x = type
-          )
-        ) %>%
-        dplyr::arrange(tissue, -scores)
-    }
-  ))
-
-sctype_scores
-
-sctype_scores$type[as.numeric(as.character(sctype_scores$scores)) < sctype_scores$ncells/4] = "Unknown"
-
-sc_sct_cluster@meta.data$customclassif <- ""
-for(j in unique(sctype_scores$cluster)) {
-  cl_type = sctype_scores[sctype_scores$cluster == j, ]
-  sc_sct_cluster@meta.data$customclassif[sc_sct_cluster@meta.data$seurat_clusters == j] = as.character(cl_type$type[1])
 }
 
-Seurat::DimPlot(
-  sc_sct_cluster,
-  reduction = "tsne",
-  label = TRUE,
-  # repel = TRUE,
-  group.by = 'customclassif'
-)  +
-  theme(
-    legend.position = "bottom"
-  )
+
+
+# stat --------------------------------------------------------------------
+
+
+project_sct %>%
+  dplyr::mutate(
+    stat = purrr::map2(
+      .x = sc,
+      .y = sct,
+      .f = fn_stat_cell
+    )
+  ) %>% 
+  dplyr::select(project, stat) %>% 
+  tidyr::unnest(cols = stat) ->
+  project_sct_reads
+
+# save stat
+writexl::write_xlsx(x = project_sct_reads, path = "data/result/01-basic/reads-stat.xlsx")
 
 
 
-# scMCA -------------------------------------------------------------------
+# Integration -------------------------------------------------------------
 
-mca <- scMCA::scMCA(
-  scdata = exp(sc_sct_cluster[["SCT"]]@scale.data),
-  numbers_plot = 3
-)
-
-mca$cors_matrix[1:4,1:3]
-mca$scMCA %>%
-  tibble::enframe() ->
-  dd
-dd %>%
-  dplyr::group_by(value) %>%
-  dplyr::count()
-
-
-ref.expr %>%
-  colnames() %>%
-  tibble::enframe() %>%
-  dplyr::mutate(cell = gsub(pattern = "\\(.*\\)", replacement = "", x = value)) %>%
-  dplyr::mutate(tissue = gsub(pattern = ".*\\(|\\)", replacement = "", x = value)) ->
-  cells
-
-cells %>%
-  dplyr::filter(grepl(pattern = "Brain", x = tissue)) %>%
-  print(n = Inf)
+project_sct %>% 
+  dplyr::select(project, sct) %>% 
+  tibble::deframe() ->
+  sct_list
