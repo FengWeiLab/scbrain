fdd1cc315d0ce3ba388f13309826dbc63cfd2948
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-06-15 23:45:44 -0400

    cell annotation

--- src/03-cell-annotation.R
+++ src/03-cell-annotation.R
@@ -13,6 +13,7 @@ library(rlang)
 library(HGNChelper)
 library(Seurat)
 library(patchwork)
+
 # Load data ---------------------------------------------------------------
 
 sc_sct_cluster <- readr::read_rds(
@@ -124,19 +125,166 @@ for(j in unique(sctype_scores$cluster)) {
   sc_sct_cluster@meta.data$customclassif[sc_sct_cluster@meta.data$seurat_clusters == j] = as.character(cl_type$type[1])
 }
 
+sc_sct_cluster$seurat_clusters <- sc_sct_cluster$integrated_snn_res.0.25
+
+pcc <- readr::read_tsv(file = "https://chunjie-sam-liu.life/data/pcc.tsv")
+sc_sct_cluster <- SetIdent(sc_sct_cluster, value = "seurat_clusters")
+
+theme_umap <- theme(
+  axis.text = element_blank(),
+  axis.ticks = element_blank(),
+  axis.title = element_text(face = "bold"),
+  # legend.position = "none"
+)
+
+fn_plot_umap <- function(.x) {
+  # .x <- sc_sct_cluster
+  .umap <- as.data.frame(.x@reductions$umap@cell.embeddings)
+  .xx <- .x@meta.data[, c("seurat_clusters", "customclassif")] %>% 
+    dplyr::rename(cluster = seurat_clusters, celltype =  customclassif)
+  .xxx <- dplyr::bind_cols(.umap, .xx)
+  .xxx %>% 
+    dplyr::select(cluster, celltype) %>% 
+    dplyr::group_by(cluster, celltype) %>%
+    dplyr::count() %>% 
+    dplyr::arrange(-n) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::group_by(cluster) %>% 
+    dplyr::top_n(1) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::select(-n) %>% 
+    dplyr::mutate(celltype = glue::glue("{cluster} {celltype}")) ->
+    .xxx_celltype
+  .xxx %>% 
+    dplyr::group_by(cluster) %>% 
+    dplyr::summarise(UMAP_1 = mean(UMAP_1), UMAP_2 = mean(UMAP_2)) %>% 
+    dplyr::left_join(.xxx_celltype, by = "cluster") ->
+    .xxx_label
+  
+  ggplot(data = .xxx) +
+    geom_point(
+      aes(
+        x = UMAP_1,
+        y = UMAP_2,
+        colour = cluster,
+        shape = NULL,
+        alpha = NULL
+      )
+    ) +
+    geom_text(
+      data = .xxx_label,
+      aes(
+        label = cluster,
+        x = UMAP_1,
+        y = UMAP_2,
+      ),
+      size = 6
+    ) +
+    scale_colour_manual(
+      name = NULL,
+      values = pcc$color,
+      labels = .xxx_label$celltype,
+      guide = guide_legend(
+        ncol = 1, 
+      )
+    ) +
+    theme(
+      panel.background = element_blank(),
+      axis.line = element_line(colour = "black"),
+      axis.ticks = element_blank(),
+      axis.text = element_blank(),
+      axis.title = element_text(size = 14, face = "bold"),
+      legend.background = element_blank(),
+      legend.key = element_blank(),
+      legend.text = element_text(size = 14)
+    ) +
+    labs(
+      x = "UMAP1",
+      y = "UMAP2"
+    )
+}
+
+p <- fn_plot_umap(.x = sc_sct_cluster)
+
+
+ggsave(
+  filename = "cluster-plot-umap.pdf",
+  plot = p,
+  device = "pdf",
+  path = "data/result/",
+  width = 20,
+  height = 10
+)
+
 Seurat::DimPlot(
   sc_sct_cluster,
   reduction = "umap",
   label = TRUE,
-  # repel = TRUE,
-  group.by = 'customclassif',
+  label.size = 6,
+  cols = pcc$color,
+  split.by = "region"
+) +
+  labs(x = "UAMP1", y = "UMAP2") +
+  theme_umap ->
+  p1;p1
+
+ggsave(
+  filename = "cluster-plot-umap-region.pdf",
+  plot = p1,
+  device = "pdf",
+  path = "data/result/",
+  width = 20,
+  height = 8
+)
+
+Seurat::DimPlot(
+  sc_sct_cluster,
+  reduction = "umap",
+  label = TRUE,
+  label.size = 6,
+  cols = pcc$color,
   split.by = "tissue",
   ncol = 3
 ) +
-  theme(
-    legend.position = "bottom"
-  ) ->
-  p
+  labs(x = "UAMP1", y = "UMAP2") +
+  theme_umap ->
+  p2;p2
+
+ggsave(
+  filename = "cluster-plot-umap-tisue.pdf",
+  plot = p2,
+  device = "pdf",
+  path = "data/result/",
+  width = 20,
+  height = 15
+)
+
+
+# Seurat::DimPlot(
+#   sc_sct_cluster,
+#   reduction = "umap",
+#   label = TRUE,
+#   label.size = 4,
+#   cols = pcc$color,
+#   group.by = 'customclassif',
+# ) +
+#   theme_umap +
+#   labs(x = "UAMP1", y = "UMAP2")  ->
+#   p2;p2
+
+# Seurat::DimPlot(
+#   sc_sct_cluster,
+#   reduction = "umap",
+#   label = TRUE,
+#   # repel = TRUE,
+#   group.by = 'customclassif',
+#   split.by = "tissue",
+#   ncol = 3
+# ) +
+#   theme(
+#     legend.position = "bottom"
+#   ) ->
+#   p
 
 ggsave(
   filename = "test-annotation-plot-map.pdf",
@@ -173,3 +321,9 @@ ref.expr %>%
 cells %>%
   dplyr::filter(grepl(pattern = "Brain", x = tissue)) %>%
   print(n = Inf)
+
+
+
+# save image --------------------------------------------------------------
+
+save.image(file = "data/rda/03-cell-annotation.rda")
