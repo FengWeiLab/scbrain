604fe89c9005c88f2ec6bc9c84a8d69598f5bb9e
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-02-06 17:26:21 -0500

    merge two data

--- src/06-merge-two-data.R
+++ src/06-merge-two-data.R
@@ -25,23 +25,57 @@ future::plan(future::multisession, workers = 10)
 
 # load data ---------------------------------------------------------------
 
-sc_sham_mcao <- readr::read_rds(file = "data/rda/project_sc.rds.gz")
 
-sc_uv <- readr::read_rds(file = "scuvdata/rda/project_uvsc.rds.gz")
-
+sc_sham_mcao_uv <- readr::read_rds(
+  file = "scuvdata/rda/project_sc_raw_all.rds.gz"
+)
 # body --------------------------------------------------------------------
 
-project_sc <- dplyr::bind_rows(
-  sc_sham_mcao,
-  sc_uv
+
+
+# Normalization -----------------------------------------------------------
+
+
+sc_sham_mcao_uv %>% 
+  dplyr::mutate(
+    scn = purrr::map(
+      .x = sc,
+      .f = function(.sc) {
+        Seurat::FindVariableFeatures(
+          object = Seurat::NormalizeData(
+            object = .sc
+          ),
+          selection.method = "vst", 
+          nfeatures = 2000
+        )
+      }
+    )
+  ) ->
+  sc_sham_mcao_uv_scn
+
+# select features taht repeatedly variable across datasets from integration
+
+features <- Seurat::SelectIntegrationFeatures(
+  object.list = sc_sham_mcao_uv_scn$scn
 )
 
+# Integration -------------------------------------------------------------
 
+anchors <- Seurat::FindIntegrationAnchors(
+  object.list = sc_sham_mcao_uv_scn$scn,
+  anchor.features = features
+)
 
-
+sc_sham_mcao_uv_scn_integrated <- Seurat::IntegrateData(
+  anchorset = anchors
+)
 
 # footer ------------------------------------------------------------------
 
 future::plan(future::sequential)
 
 # save image --------------------------------------------------------------
+
+save.image(
+  file = "scuvdata/rda/project_uvsc.rda"
+)
