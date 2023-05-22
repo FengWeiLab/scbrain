8a70e11e3045ac5446ea5dfaef8a5b7576b39815
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2023-05-21 22:15:52 -0400

    differences

--- azimuth/04-manual-annotation.R
+++ azimuth/04-manual-annotation.R
@@ -11,6 +11,7 @@ library(magrittr)
 library(ggplot2)
 library(patchwork)
 library(rlang)
+library(Seurat)
 
 # args --------------------------------------------------------------------
 
@@ -20,20 +21,54 @@ library(rlang)
 
 # header ------------------------------------------------------------------
 
-future::plan(future::multisession, workers = 10)
+# future::plan(future::multisession, workers = 10)
 
 # function ----------------------------------------------------------------
 
 
 # load data ---------------------------------------------------------------
-
+azimuth_ref <- readr::read_rds(
+  file = "/home/liuc9/github/scbrain/data/azimuth/project_sc_azimuth_ref_realcell_sunburst.rds"
+)
 
 # body --------------------------------------------------------------------
+
+
+azimuth_ref$anno[[3]] |>
+  Seurat::RunPCA() |>
+  Seurat::RunTSNE() |>
+  Seurat::RunUMAP(dims = 1:30) ->
+  a
+
+a$predicted.celltype.l2
+Idents(a) <- a$predicted.celltype.l1
+
+
+FindMarkers(
+  a,
+  ident.1 = "CD8 T",
+  ident.2 = "B",
+)
+
+
+DimPlot(
+  a,
+  group.by = "predicted.celltype.l2",
+  reduction = "ref.umap"
+)
+
+DimHeatmap(a, dims = 2)
+
+DimPlot(
+  azimuth_ref$anno_new[[3]],
+  group.by = "predicted.celltype.l2"
+)
+
 
 
 
 # footer ------------------------------------------------------------------
 
-future::plan(future::sequential)
+# future::plan(future::sequential)
 
 # save image --------------------------------------------------------------
